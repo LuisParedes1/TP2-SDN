ffb0f9c2633709727c57f92309c2d89f0ceb135d
Danikim01 <dakim@fi.uba.ar>
2023-11-14 02:26:17 -0300

    correccion pingall funcionando

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 Trabajo Practico 2 de Introduccion a Sistemas Distribuidos
 
+# Pasos
+
+1) git clone https://github.com/noxrepo/pox.git
+2) export PATH=$PATH:/home/daniel/Desktop/TP2 - OpenFlow/TP2-SDN/pox
+3) Ejecutar:  python3 pox.py forwarding.l2_learning 
+
 # Comando para ejectuar la topologia personalizada
+## Ejecutar en otra terminal:
 
 sudo mn --custom example.py --topo mytopo --mac --switch ovsk --controller remote
 
--- example.py
+++ example.py
@@ -7,12 +7,10 @@ class MyTopo( Topo ):
         # Add hosts and switches
         leftHost = self.addHost( 'h1' )
         rightHost = self.addHost( 'h2' )
-        leftSwitch = self.addSwitch( 's3' )
-        rightSwitch = self.addSwitch( 's4' )
+        switch = self.addSwitch( 's1' )
         # Add links
-        self.addLink( leftHost, leftSwitch )
-        self.addLink( leftSwitch, rightSwitch )
-        self.addLink( rightSwitch, rightHost )
+        self.addLink( leftHost, switch )
+        self.addLink( switch, rightHost )
 
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
