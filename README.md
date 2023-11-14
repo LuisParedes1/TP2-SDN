31ca6dac9254b637e9ebe3ae7b265955c1a5676c
Danikim01 <dakim@fi.uba.ar>
2023-11-14 16:42:43 -0300

    parametrizando la cantidad de switches

--- README.md
+++ README.md
@@ -5,13 +5,20 @@ Trabajo Practico 2 de Introduccion a Sistemas Distribuidos
 # Pasos
 
 1) git clone https://github.com/noxrepo/pox.git
-2) export PATH=$PATH:/home/daniel/Desktop/TP2 - OpenFlow/TP2-SDN/pox
-3) Ejecutar:  python3 pox.py forwarding.l2_learning 
+2) Ejecutar:  python3 pox/pox.py forwarding.l2_learning 
 
 # Comando para ejectuar la topologia personalizada
 ## Ejecutar en otra terminal:
 
-sudo mn --custom example.py --topo mytopo --mac --switch ovsk --controller remote
+n = cantidad de switches (tiene que ser mayor a 1)
+
+### Ejecucion sin xterm
+
+sudo mn --custom example.py --topo mytopo,n --mac --arp --switch ovsk --controller remote
+
+### Ejecucion con xterm
+
+sudo mn --custom example.py --topo mytopo,n --mac --arp -x --switch ovsk --controller remote
 
 # Notion
 [TP2](https://mis-notas.notion.site/TP2-0c7f3987e3324e289050206e3edb01a4?pvs=4)
--- example.py
+++ example.py
@@ -1,17 +1,33 @@
 from mininet.topo import Topo
 
-class MyTopo( Topo ):
-    def __init__( self ):
-        # Initialize topology
-        Topo.__init__( self )
-        # Add hosts and switches
-        leftHost = self.addHost( 'h1' )
-        rightHost = self.addHost( 'h2' )
-        switch = self.addSwitch( 's1' )
-        # Add links
-        self.addLink( leftHost, switch )
-        self.addLink( switch, rightHost )
+class MyTopo(Topo):
+    def __init__(self, n_switches=1, **opts):
+        super(MyTopo, self).__init__(**opts)
 
+        if n_switches < 1:
+            raise Exception("number of switches must be at least 1")
 
-topos = { 'mytopo': ( lambda: MyTopo() ) }
+        h1 = self.addHost('host_1')
+        h2 = self.addHost('host_2')
+        h3 = self.addHost('host_3')
+        h4 = self.addHost('host_4')
+
+        s1 = self.addSwitch('switch_1')
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+
+        if n_switches == 1:
+            self.addLink(h3, s1)
+            self.addLink(h4, s1)
+            return
+        else:
+            for i in range(2, n_switches + 1):
+                s2 = self.addSwitch('switch_' + str(i))
+                self.addLink(s1, s2)
+                s1 = s2
+            self.addLink(s2, h3)
+            self.addLink(s2, h4)
+
+topos = {'mytopo': MyTopo}
+
 
