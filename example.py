4f28d1bfc56941bef093810dec32c583d4aa33af
Nox <56934023+noxethiems@users.noreply.github.com>
2023-11-20 18:24:25 -0300

    Topologia inicial (#1)
    * implemento topologia inicial
    
    * correccion del comando para correr la topologia
    
    * correccion pingall funcionando
    
    * parametrizando la cantidad de switches
    
    * agrego codigo base para firewall.py
    
    * reglas definidas e implementacion inicial firewall
    
    * README.md completado
    
    * cambios en rules.json, agregue mas reglas
    
    ---------
    
    Co-authored-by: Danikim01 <dakim@fi.uba.ar>

---
+++ example.py
@@ -0,0 +1,33 @@
+from mininet.topo import Topo
+
+class MyTopo(Topo):
+    def __init__(self, n_switches=1, **opts):
+        super(MyTopo, self).__init__(**opts)
+
+        if n_switches < 1:
+            raise Exception("number of switches must be at least 1")
+
+        h1 = self.addHost('host_1')
+        h2 = self.addHost('host_2')
+        h3 = self.addHost('host_3')
+        h4 = self.addHost('host_4')
+
+        s1 = self.addSwitch('switch_1')
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+
+        if n_switches == 1:
+            self.addLink(h3, s1)
+            self.addLink(h4, s1)
+            return
+        else:
+            for i in range(2, n_switches + 1):
+                s2 = self.addSwitch('switch_' + str(i))
+                self.addLink(s1, s2)
+                s1 = s2
+            self.addLink(s2, h3)
+            self.addLink(s2, h4)
+
+topos = {'mytopo': MyTopo}
+
+
